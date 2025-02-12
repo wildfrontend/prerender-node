7304e4384f965b7d1470a1a0173ddf771115b52c
Louis Luo <louis.luo@gosugamers.net>
2025-02-12 15:44:59 +0800

    Update robots.txt

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,13 @@ const port = process.env.PORT || 3000;
 
 app.use(require('prerender-node'));
 
+app.use((req, res, next) => {
+  console.log('User-Agent:',req.get('User-Agent'))
+  return next()
+});
+
 app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.use(express.static(path.join(__dirname, '../static')))
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
